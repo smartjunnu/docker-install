e6c1567a381dfe9529f80f13abf7f16c95ffe4d1
smartjunnu <113268958+smartjunnu@users.noreply.github.com>
2023-07-20 22:37:55 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -3,12 +3,12 @@
 cat $HOME/.ssh/authorized_keys >> authorized_keys
 for i in `cat hosts`
 do
- ssh -i key.pem kafkaadmin@$i "echo -e 'y\n' | ssh-keygen -t rsa -P '' -f $HOME/.ssh/id_rsa"
- ssh -i key.pem kafkaadmin@$i 'touch ~/.ssh/config; echo -e \ "host *\n StrictHostKeyChecking no\n UserKnownHostsFile=/dev/null" \ > ~/.ssh/config; chmod 644 ~/.ssh/config'
- ssh -i key.pem kafkaadmin@$i 'cat $HOME/.ssh/id_rsa.pub' >> authorized_keys
+ ssh -i key.pem centos@$i "echo -e 'y\n' | ssh-keygen -t rsa -P '' -f $HOME/.ssh/id_rsa"
+ ssh -i key.pem centos@$i 'touch ~/.ssh/config; echo -e \ "host *\n StrictHostKeyChecking no\n UserKnownHostsFile=/dev/null" \ > ~/.ssh/config; chmod 644 ~/.ssh/config'
+ ssh -i key.pem centos@$i 'cat $HOME/.ssh/id_rsa.pub' >> authorized_keys
 done
 
 for i in `cat hosts`
 do
- scp -i key.pem authorized_keys kafkaadmin@$i:$HOME/.ssh/authorized_keys
+ scp -i key.pem authorized_keys centos@$i:$HOME/.ssh/authorized_keys
 done
